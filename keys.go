91457661307f0bf38303c33136e2477001db478d
faiface <faiface2202@gmail.com>
2019-05-03 23:42:57 +0200

    add key constants

---
+++ keys.go
@@ -0,0 +1,21 @@
+package gui
+
+const (
+	KeyLeft int = iota
+	KeyRight
+	KeyUp
+	KeyDown
+	KeyEscape
+	KeySpace
+	KeyBackspace
+	KeyDelete
+	KeyEnter
+	KeyTab
+	KeyHome
+	KeyEnd
+	KeyPageUp
+	KeyPageDown
+	KeyShift
+	KeyCtrl
+	KeyAlt
+)
